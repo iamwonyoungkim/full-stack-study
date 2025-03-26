3cb1403640bf6317bd46bbf051c8edac88211514
Wonyoung Kim <wonyoung020626@gmail.com>
2025-03-26 09:25:46 +0900

    Daily exercise : basic
    25 Mar

---
+++ KB-IT's-Your-Life/practical/02_Vue/02_Vue/exercise-25Mar/basic/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li><input type="checkbox" :checked="checked" />{{ name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxItem',
+  props: {
+    id: [Number, String],
+    name: String,
+    checked: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
