ff8f37c03c9417af5bd876cfdd127d295cf19e25
Wonyoung Kim <wonyoung020626@gmail.com>
2025-03-26 09:24:48 +0900

    lecture : Single File Component
    25 Mar

---
+++ KB-IT's-Your-Life/lecture/02_Vue/02_Vue/ch07/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li><input type="checkbox" v-model="checked" /></li>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxItem',
+  data() {
+    return {
+      checked: false,
+    };
+  },
+};
+</script>
