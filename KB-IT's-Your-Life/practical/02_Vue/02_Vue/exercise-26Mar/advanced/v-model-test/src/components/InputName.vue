a112fcd2c9f39ad8087d817bc125963dbaf8e76c
Wonyoung Kim <wonyoung020626@gmail.com>
2025-03-27 09:19:40 +0900

    daily exercise : advanced

---
+++ KB-IT's-Your-Life/practical/02_Vue/02_Vue/exercise-26Mar/advanced/v-model-test/src/components/InputName.vue
@@ -0,0 +1,16 @@
+<template>
+  <input
+    type="text"
+    :value="name"
+    @input="$emit('update:name', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'InputName',
+  props: {
+    name: '',
+  },
+};
+</script>
