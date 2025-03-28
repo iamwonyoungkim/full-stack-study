3592b9f074f8dc5cd413e408936006824469dffb
Wonyoung Kim <wonyoung020626@gmail.com>
2025-03-28 09:27:55 +0900

    Daily exercise : basic

---
+++ KB-IT's-Your-Life/practical/02_Vue/02_Vue/exercise-27Mar/basic/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    <div>결과 : {{ result }}</div>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  name: 'Calc',
+  setup() {
+    const x = ref(0);
+    const result = ref(0);
+
+    watch(x, (newValue, oldValue) => {
+      console.log(`이전 값: ${oldValue}, 새 값: ${newValue}`);
+      result.value = newValue * 2;
+    });
+
+    return { x, result };
+  },
+};
+</script>
