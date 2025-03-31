c72b3856b335648c2a402326f50456dd102db752
Wonyoung Kim <wonyoung020626@gmail.com>
2025-03-31 17:50:44 +0900

    Daily exercise
    28 Mar

---
+++ KB-IT's-Your-Life/practical/02_Vue/02_Vue/exercise-28Mar/router-test/src/components/MemberInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card card-body">
+    <h2>Member Info</h2>
+    <div>
+      경로 패턴: /members/:id<br />
+      요청 경로: {{ currentRoute.fullPath }}<br />
+      id값: {{ currentRoute.params }}
+    </div>
+  </div>
+</template>
+<script>
+import { useRoute } from 'vue-router';
+import members from '@/members.json';
+
+export default {
+  name: 'MemberInfo',
+  setup() {
+    const currentRoute = useRoute();
+    const id = parseInt(currentRoute.params.id, 10);
+    const member = members.find((m) => m.id === id);
+    return { member, currentRoute };
+  },
+};
+</script>
---
+++ KB-IT's-Your-Life/practical/02_Vue/02_Vue/exercise-28Mar/router-test/src/pages/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <!-- Options API -->
+    <!-- <p>요청 경로 : {{ $route.fullPath }}</p> -->
+
+    <!-- Composition API -->
+    <p>요청 경로 : {{ currentRoute.fullPath }}</p>
+  </div>
+</template>
+
+<!-- Options API -->
+<!-- <script>
+export default {
+  name: 'About',
+  created() {
+    console.log(this.$route);
+  },
+};
+</script> -->
+
+<!-- Composition API -->
+<!-- <script>
+import { useRoute } from 'vue-router';
+
+export default {
+  name: 'About',
+  setup() {
+    const currentRoute = useRoute();
+    console.log(currentRoute.fullPath);
+    return { currentRoute };
+  },
+};
+</script> -->
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const currentRoute = useRoute();
+console.log(currentRoute);
+</script>
