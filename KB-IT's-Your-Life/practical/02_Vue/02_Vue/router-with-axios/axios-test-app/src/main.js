0ab5276b9f66b0a739102c2890a76e1acef9a42b
Wonyoung Kim <wonyoung020626@gmail.com>
2025-04-04 13:12:09 +0900

    made exercise directory

--- KB-IT's-Your-Life/practical/02_Vue/02_Vue/router-with-axios/axios-test-app/src/main.js
+++ KB-IT's-Your-Life/practical/02_Vue/02_Vue/router-with-axios/axios-test-app/src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createPinia } from 'pinia';
 
+// Pinia 스토어 생성
+const pinia = createPinia();
+
+// Vue 애플리케이션 생성
 const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
 
+// 앱 마운트
 app.mount('#app');
