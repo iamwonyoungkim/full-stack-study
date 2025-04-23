d5bcc40cc65339c7b57ba93aeb21ec145d6b1e0c
Wonyoung Kim <wonyoung020626@gmail.com>
2025-04-23 22:48:13 +0900

    Self-learning : Java - Handling Exception
    23 Apr

---
+++ self-learning/java/thisisjava/src/ch11/sec02/exam01/ExceptionHandlingExample2.java
@@ -0,0 +1,28 @@
+package ch11.sec02.exam01;
+
+public class ExceptionHandlingExample2 {
+
+  public static void printLength(String data) {
+    try {
+      int result = data.length();
+      System.out.println("문자 수: " + result);
+    } catch (NullPointerException e) {
+      // 예외 정보를 얻는 3가지 방법 (출력 내용 다름)
+      System.out.println(e.getMessage()); // 예외가 발생한 이유만 리턴
+//      System.out.println(e.toString()); // 예외의 종류도 리턴
+//      e.printStackTrace(); // 예외가 어디서 발생했는지 추적한 내용도 리턴
+    } finally {
+      System.out.println("[마무리 실행]\n");
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("[프로그램 시작]\n");
+    printLength("ThisIsJava");
+    printLength(null);
+    System.out.println("[프로그램 종료]");
+  }
+
+
+}
