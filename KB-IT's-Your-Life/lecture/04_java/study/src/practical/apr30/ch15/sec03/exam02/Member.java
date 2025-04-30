1343796e3d01ae8d8c6051a17f557c67d374af31
Wonyoung Kim <wonyoung020626@gmail.com>
2025-04-30 17:19:26 +0900

    Practical : Exercise(Basic)
    30 Apr

---
+++ KB-IT's-Your-Life/lecture/04_java/study/src/practical/apr30/ch15/sec03/exam02/Member.java
@@ -0,0 +1,26 @@
+package practical.apr30.ch15.sec03.exam02;
+
+import java.util.Objects;
+
+public class Member {
+  public String name;
+  public int age;
+
+  public Member(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Member member)) {
+      return false;
+    }
+    return age == member.age && Objects.equals(name, member.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, age);
+  }
+}
