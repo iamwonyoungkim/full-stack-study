e6f505a3a4390bf55d4734efb10f65d13cc4da62
Wonyoung Kim <wonyoung020626@gmail.com>
2025-05-10 16:35:32 +0900

    feature : Add router-link and router-view for searchPage and favouritesPage

--- personal-project/book-project/src/App.vue
+++ personal-project/book-project/src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <FavouritesPage />
-  <SearchPage />
+  <div>
+    <nav class="navbar navbar-expand navbar-light bg-light px-3 mb-3">
+      <router-link to="/" class="nav-link pe-3">도서 검색</router-link>
+      <router-link to="/favourites" class="nav-link">즐겨찾기</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
