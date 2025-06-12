eb159fa4121894bc3add8f476f01ae0e351ef7c1
Wonyoung Kim <wonyoung020626@gmail.com>
2025-06-12 10:04:40 +0900

    Lecture : update SpringLegacy template

--- KB-ITs-Your-Life/lecture/08_Spring/SpringLegacy/src/main/java/org/scoula/config/ServletConfig.java
+++ KB-ITs-Your-Life/lecture/08_Spring/SpringLegacy/src/main/java/org/scoula/config/ServletConfig.java
@@ -1,6 +1,9 @@
 package org.scoula.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -10,7 +13,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 // FrontController(DispatcherServlet), Web MVC 관련 설정
 @EnableWebMvc
-@ComponentScan(basePackages = {"org.scoula.controller"}) // Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
+@ComponentScan(basePackages = {"org.scoula.exception","org.scoula.controller"}) // Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
 public class ServletConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -31,5 +34,12 @@ public class ServletConfig implements WebMvcConfigurer {
         bean.setSuffix(".jsp");
 
         registry.viewResolver(bean);
+    }
+
+    // Servlet 3.0 파일 업로드 사용시
+    @Bean
+    public MultipartResolver multipartResolver() {
+        StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+        return resolver;
     }
 }
---
+++ KB-ITs-Your-Life/lecture/08_Spring/SpringLegacy/src/main/webapp/WEB-INF/views/custom404.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Home
+  Date: 2025-06-11
+  Time: 오후 10:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+  <h1>해당 URL(${uri})은 존재하지 않습니다.</h1>
+</body>
+</html>
