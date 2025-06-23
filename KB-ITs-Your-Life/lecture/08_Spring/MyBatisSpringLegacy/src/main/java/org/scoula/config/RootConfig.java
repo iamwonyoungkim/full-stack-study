7ca601b1b3671b09ff82dd5ed6c76f60d89121bd
Wonyoung Kim <wonyoung020626@gmail.com>
2025-06-23 11:55:54 +0900

    Edit MyBatisSpringLegacy template

--- KB-ITs-Your-Life/lecture/08_Spring/MyBatisSpringLegacy/src/main/java/org/scoula/config/RootConfig.java
+++ KB-ITs-Your-Life/lecture/08_Spring/MyBatisSpringLegacy/src/main/java/org/scoula/config/RootConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -18,6 +19,7 @@ import javax.sql.DataSource;
 // 일반 설정. 어디에서든지 써먹을 설정들
 @Configuration
 @PropertySource({"classpath:/application.properties"})
+@ComponentScan(basePackages = {})
 @MapperScan(basePackages = {})
 public class RootConfig {
     @Value("${jdbc.driver}") String driver;
