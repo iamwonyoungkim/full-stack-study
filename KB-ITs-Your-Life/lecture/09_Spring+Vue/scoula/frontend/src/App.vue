105d2ecaa27d1391470596b33e425aa4f91408b4
Wonyoung Kim <wonyoung020626@gmail.com>
2025-06-27 16:28:17 +0900

    lecture : set up for the project(frontend)

---
+++ KB-ITs-Your-Life/lecture/09_Spring+Vue/scoula/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import DefaultLayout from './components/layouts/DefaultLayout.vue';
+</script>
+
+<template>
+  <DefaultLayout>
+    <!-- 이게 DefaultLayout.vue의 slot 자리에 들어가게 된다. -->
+    <RouterView />
+  </DefaultLayout>
+</template>
+
+<style scoped></style>
---
+++ KB-ITs-Your-Life/lecture/09_Spring+Vue/scoula/frontend/src/components/layouts/menu/LogoutMenuItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const logout = (e) => {
+  // 로그아웃
+  router.push('/');
+};
+</script>
+
+<template>
+  <!-- 
+    @click.prevent : bubbling을 막는다. / default 이벤트를 막는다.
+    @click.stop : capturing을 막는다.
+    - bubbling: 이벤트가 하위 요소에서 상위 요소로 전파되는 것
+    - capturing: 이벤트가 상위 요소에서 하위 요소로 전파되는 것
+    -->
+  <a href="#" class="nav-link" @click.prevent="logout">
+    <i class="fa-solid fa-right-from-bracket"></i>
+    로그아웃
+  </a>
+</template>
