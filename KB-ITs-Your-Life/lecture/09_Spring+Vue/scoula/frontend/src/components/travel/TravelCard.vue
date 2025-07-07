c692a3321bd7aeda4dc619cb8232d53993607c2d
Wonyoung Kim <wonyoung020626@gmail.com>
2025-07-07 13:03:53 +0900

    lecture : travel (frontend)

---
+++ KB-ITs-Your-Life/lecture/09_Spring+Vue/scoula/frontend/src/components/travel/TravelCard.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { useRoute } from 'vue-router';
+const cr = useRoute();
+
+const props = defineProps({
+  travel: { Type: Object, required: true },
+});
+</script>
+
+<template>
+  <!-- grid 폭에 맞춤 -->
+  <div class="card" style="width: 100%">
+    <router-link
+      :to="{
+        name: 'travel/detail',
+        params: { no: travel.no },
+        query: cr.query,
+      }"
+    >
+      <img
+        :src="travel.images[0].url"
+        :alt="travel.title"
+        class="card-img-top"
+      />
+    </router-link>
+
+    <div class="card-body">
+      <h5 class="card-title">
+        <router-link
+          :to="{
+            name: 'travel/detail',
+            params: { no: travel.no },
+            query: cr.query,
+          }"
+          >{{ travel.title }}</router-link
+        >
+      </h5>
+      <p class="card-text">{{ travel.description.substring(0, 250) }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-img-top {
+  height: 200px;
+  /* 꽉 채워라. 튀어나가는 부분은 자른다. */
+  object-fit: cover;
+}
+
+.card-text {
+  /* 넘치는 부분은 숨겨라 */
+  overflow: hidden;
+  /* ellipsis -> 말 줄임 */
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  /* 줄 수 */
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
--- KB-ITs-Your-Life/lecture/09_Spring+Vue/scoula/frontend/src/router/travel.js
+++ KB-ITs-Your-Life/lecture/09_Spring+Vue/scoula/frontend/src/router/travel.js
@@ -4,4 +4,9 @@ export default [
     name: 'travel/list',
     component: () => import('../pages/travel/TravelListPage.vue'), // Lazy-loaded component
   },
+  {
+    path: '/travel/detail/:no',
+    name: 'travel/detail',
+    component: () => import('../pages/travel/TravelDetailPage.vue'), // Lazy-loaded component
+  },
 ];
